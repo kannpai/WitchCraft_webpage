c0345ad245a52f422640ee38506b4b6b3274cf2c
gan_pei <gan_pei@yahoo.com>
2018-11-28 21:24:12 +0000

    change pages
    structure the pages

--- js/MapDisplay.js
+++ js/MapDisplay.js
@@ -131,20 +131,3 @@ var resetMap = function(){
 
 
 
-     $("#button1").click(function(){
-
-		if($("#illu1").css("visibility")!="hidden"){
-		    $("#illu1").css("visibility","hidden");
-		}else{
-		    $("#illu1").css("visibility","visible");
-		};
-     });
-
-     $("#button2").click(function(){
-
-		if($("#illu2").css("visibility")!="hidden"){
-		    $("#illu2").css("visibility","hidden");
-		}else{
-		    $("#illu2").css("visibility","visible");
-		};
-     });
